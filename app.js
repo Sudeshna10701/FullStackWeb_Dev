a62acef4fd357198592461db28d1dbaf1435b702
Sudeshna10701 <sudeshna10dutta@gmail.com>
2021-10-01 19:33:56 +0530

    switch

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
-console.log("Hello World")
-console.warn("hello warning")
+const a = document.querySelectorAll('input')[0]
+const b = document.querySelectorAll('input')[1]
+const add = document.querySelector('#add')
+const multiply = document.querySelector('#multiply')
+const resultBox = document.querySelector('.result')
 
-console.log(typeof 2)
+const sum = () => {
+    const result = parseInt(a.value) + parseInt(b.value)
+    resultBox.innerHTML = result
+}
 
-console.log(typeof true)
+const product = () => {
+    const result = parseInt(a.value) * parseInt(b.value)
+    resultBox.innerHTML = result
+}
+
+
+const calculate = (event, operation) => {
+   
+    // if (operation == 'add') {
+        // console.log('adding')
+    // } else if (operation == 'multiply') {
+        // console.log('multiply')
+    // }
+    switch(operation){
+    case "add":
+      resultBox.innerHTML=parseInt(a.value)=parseInt(b.value)
+      break
+    case "multiply":
+      resultBox.innerHTML=parseInt(a.value)=parseInt(b.value)
+      break
+    case "subtract":
+        resultBox.innerHTML=parseInt(a.value)=parseInt(b.value)
+        break  
+    default:
+      resultBox.innerHTML="not a valid operation"
+    }
+}
+
+
+add.addEventListener('click', (event) => {
+    calculate(event, 'add')
+})
+
+
+multiply.addEventListener('click', (event) => {
+    calculate(event, 'multiply')
+})

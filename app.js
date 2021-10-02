1128f2e545fad98f1acaa0f27b90add28c399577
Sudeshna10701 <sudeshna10dutta@gmail.com>
2021-10-02 19:33:27 +0530

    Dom

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-var heading = document.getElementById('heading')
-var para = document.getElementsByClassName('para')
-var tag = document.getElementsByTagName('a')
-var query = document.querySelector('.para')
+// var heading = document.getElementById('heading')
+// var body = document.querySelector('body')
 
-//console.log(heading)
-// console.log(para.length)
-// console.log(para[0])
-// console.log(para[1])
+// console.log[heading]
 
-// para.push('asdf')
+// heading.innerHTML = "i was added by JS"
+// heading.style = "color:red;font-size:2rem"
+// heading.classList.add('five')
+// heading.classList.remove('one')
+// console.log(heading.classList)
 
-console.log(query)
+// body.classList.add('dark')
+// body.classList.remove('dark')
+var list = document.querySelector('ul')
+ console.log(list)
+ var arr = ['Go to the gym','cook','test']
+
+ for(var i=0; i= arr.length;i++){
+   var element = document.createElement('li')
+   var textnode = document.createTextNode(arr[i])
+   console.log(element)
+   element.appendChild(textnode)
+   console.log(element)
+ }
